4f7f407743459fa8b5e52fd8f0c103b826103bae
Lup1in <gabrielcunhaleite@hotmail.com>
2022-08-13 12:06:54 -0300

    consulta simples com @query

--- src/main/java/com/gabriel/workshopmongo/services/PostService.java
+++ src/main/java/com/gabriel/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.gabriel.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> searchTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
 }
